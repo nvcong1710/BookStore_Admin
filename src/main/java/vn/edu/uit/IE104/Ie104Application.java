81ecf105bd02403cc2fb3de6c9a9305e9132bfef
Nguyễn Viết Công <91941271+nvcong1710@users.noreply.github.com>
2024-09-29 20:38:23 +0700

    first commit

---
+++ src/main/java/vn/edu/uit/IE104/Ie104Application.java
@@ -0,0 +1,13 @@
+package vn.edu.uit.IE104;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ie104Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ie104Application.class, args);
+	}
+
+}
