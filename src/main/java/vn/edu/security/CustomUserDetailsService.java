526ff449d124dcd742917d265073bdf6c926e383
nguyenchuthanhduy <chuthanhduy242@gmail.com>
2024-10-03 20:47:14 +0700

    fix -verified user before login

---
+++ src/main/java/vn/edu/security/CustomUserDetailsService.java
@@ -0,0 +1,23 @@
+package vn.edu.uit.IE104.security;
+
+
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import vn.edu.uit.IE104.entity.User;
+import vn.edu.uit.IE104.repository.UserRepository;
+
+
+@Service
+@AllArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+   private final UserRepository userRepository;
+
+
+   @Override
+   public CustomUserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+       User user = userRepository.findByEmail(email);
+       return CustomUserDetails.buildUserDetails(user);
+   }
+}
